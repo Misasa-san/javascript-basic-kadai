18cb839ea460ed37776e4a5e3cf6036bc7dad955
Misasa-san <misasa@tokyo-gas.co.jp>
2025-07-30 09:31:39 +0900

    課題021の提出

---
+++ kadai_021/event.js
@@ -0,0 +1,11 @@
+// btnというidのHTML要素を取得し、定数に代入
+const btn = document.getElementById('btn');
+
+// textというidのHTML要素を取得し、定数に代入
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+    setTimeout(() => {
+        text.textContent = 'ボタンをクリックしました';
+}, 2000);
+});
